dec503e544b73520d56b7228f734a9fd2c6081f3
Danyel Granzotti <danyel.granzotti17@gmail.com>
2024-09-07 15:02:58 -0300

    fix: docker back

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "example",
-  database: process.env.DB_NAME || "task_manager_db",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 db.connect((err) => {
